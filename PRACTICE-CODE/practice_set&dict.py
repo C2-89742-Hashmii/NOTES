04cd13b014e22a9bde9e1af48bd9c031fd03c45d
Saiif_hashmii <162566676+Saiifhashmii@users.noreply.github.com>
2025-03-02 22:52:07 +0530

    _

--- PRACTICE-CODE/practice_set&dict.py
+++ PRACTICE-CODE/practice_set&dict.py
@@ -4,8 +4,41 @@
 # print(type (res))
 
 
-res=[1,2,3,4,5,6,7,8,5,6]
-res1=set(res)
+# res=[1,2,3,4,5,6,7,8,5,6]
+# res1=set(res)
 
 
-res=[]
+# res=[]
+
+
+# list1={1,2,3,4,5}
+# result=set(map(lambda n:n*2,list1))
+# print(result)
+
+# string="saiif"
+# print (string[:len(string)])
+# print (string[::-1])
+
+###################################DICTIONARY##############################
+
+# DIC={
+#     "dgmnn":"dbsfbm",
+#     "fbmxg":"gfbzb"
+# }
+# # print(DIC)
+
+# # print(DIC["dgmnn"])  # direct access krne pr agar key na ho dict me to error throw hoga 
+# # print(DIC.get("dgmnnn"))  # by this .get method it return None if value dict me na ho to 
+
+# for key in DIC.keys():
+#     print(DIC[key])
+#     # print value attached to key 
+#     print(key)  
+#     # it directly print key 
+    
+# a={} #empty dic
+# b=set() #empty set
+# c=() #empty tupple
+# print(type(a))
+# print(type(b))
+# print(type(c))
